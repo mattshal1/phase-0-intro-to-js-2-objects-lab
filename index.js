c949f06ff5d9993e9709208651c0e5e457fbc06a
Ahmed Shalash <ah.shalash@gmail.com>
2023-10-13 13:19:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+let employee = {
+    name: 'Sam',
+    age: 28
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, [key]: value};
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  } 
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const cloned = {...employee};
+    delete cloned[key];
+    return cloned;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]; 
+    return employee;
+  }
+  
+  module.exports = {
+    employee,
+    updateEmployeeWithKeyAndValue,
+    destructivelyUpdateEmployeeWithKeyAndValue,
+    deleteFromEmployeeByKey,
+    destructivelyDeleteFromEmployeeByKey  
+  };
